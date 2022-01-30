94c3d9030414ab70437ce3a9f515d5a975671ab9
Karim Hassan <karimhassan92@gmail.com>
2022-01-29 22:23:14 -0500

    update

--- main.js
+++ main.js
@@ -1,90 +1,78 @@
-let numbers = document.querySelectorAll(".num");
-let operations = document.querySelectorAll(".ops");
-let strikes = 0
 document.querySelector(".clear").addEventListener("click", clearAll);
 document.querySelector(".equals").addEventListener("click", equals);
-document.getElementById("");
-let arr = [];
-let sum = 0;
-let reference = "";
+let numbers = document.querySelectorAll(".num"),
+  operations = document.querySelectorAll(".ops");
+let strikes = 0,
+  reducerArray = [],
+  sum = 0,
+  reference = "";
 Array.from(numbers).forEach(function (element) {
   element.addEventListener("click", function (e) {
     sum += e.target.dataset.num;
     document.querySelector(".viewer").innerHTML = +sum;
-    console.log(+sum)
-    if(isNaN(+sum)){
-        clearAll()
-        strikes++
-        handleErrors()
-        alert(`You either divided by 0 or double clicked the decimal, thats strike ${strikes}`)
+    if (isNaN(+sum) || +sum === Number.POSITIVE_INFINITY) {
+      clearAll();
+      strikes++;
+      if (strikes === 3) {
+        handleErrors();
+      } else {
+        alert(
+          `You either divided by 0 or double clicked the decimal, thats strike ${strikes}`
+        );
+      }
     }
   });
 });
-console.log(document.querySelector(".viewer").innerText)
 Array.from(operations).forEach(function (element) {
   element.addEventListener("click", function (e) {
     if (reference === "plus" || reference === "minus") {
-        arr.push(+sum || 0);
-        if(reference === 'plus'){
-        arr = [arr.reduce((a,b)=> a + b, 0)]
-        document.querySelector(".viewer").innerHTML = arr[0].toFixed(2);
-        }
-        else{
-            arr = [arr.reduce((a,b)=> a - b)]
-            document.querySelector(".viewer").innerHTML = arr[0].toFixed(2);
-        }
+      reducerArray.push(+sum || 0);
+      if (reference === "plus") {
+        reducerArray = [reducerArray.reduce((a, b) => a + b, 0)];
       } else {
-        arr.push(+sum || 1);
-        if(reference === 'times'){
-            arr = [arr.reduce((a,b)=> a * b, 1)]
-            document.querySelector(".viewer").innerHTML = arr[0].toFixed(2);
-        }
-        if(reference === 'divided'){
-            arr = [arr.reduce((a,b)=> a / b)]
-            document.querySelector(".viewer").innerHTML = arr[0].toFixed(2);
-        }
+        reducerArray = [reducerArray.reduce((a, b) => a - b)];
       }
-      reference = e.target.dataset.ops
+    } else {
+      reducerArray.push(+sum || 1);
+      if (reference === "times") {
+        reducerArray = [reducerArray.reduce((a, b) => a * b, 1)];
+      }
+      if (reference === "divided") {
+        reducerArray = [reducerArray.reduce((a, b) => a / b)];
+      }
+    }
+    document.querySelector(".viewer").innerHTML = reducerArray[0];
+    reference = e.target.dataset.ops;
     sum = 0;
-    console.log(arr)
   });
 });
 
 function equals() {
   if (reference === "times") {
-    arr.push(+sum);
-    if(arr.includes(NaN)){
-        clearAll()
-    }
-    arr = [arr.reduce((a, b) => a * b, 1)];
-    document.querySelector(".viewer").innerHTML = arr[0].toFixed(2);
+    reducerArray.push(+sum);
+    reducerArray = [reducerArray.reduce((a, b) => a * b, 1)];
+    document.querySelector(".viewer").innerHTML = reducerArray[0];
     sum = 0;
   } else if (reference === "divided") {
-    arr.push(+sum);
-    arr = [arr.reduce((a, b) => a / b)];
-    document.querySelector(".viewer").innerHTML = arr[0].toFixed(2);
+    reducerArray.push(+sum);
+    reducerArray = [reducerArray.reduce((a, b) => a / b)];
+    document.querySelector(".viewer").innerHTML = reducerArray[0];
     sum = 0;
   } else if (reference === "plus") {
-    arr.push(+sum);
-    arr = [arr.reduce((a, b) => a + b, 0)];
-    document.querySelector(".viewer").innerHTML = arr[0].toFixed(2);
+    reducerArray.push(+sum);
+    reducerArray = [reducerArray.reduce((a, b) => a + b, 0)];
+    document.querySelector(".viewer").innerHTML = reducerArray[0];
     sum = 0;
   } else if (reference === "minus") {
-    arr.push(+sum);
-    arr = [arr.reduce((a, b) => a - b)];
-    document.querySelector(".viewer").innerHTML = arr[0].toFixed(2);
-    sum = 0;
+    reducerArray.push(+sum);
+    reducerArray = [reducerArray.reduce((a, b) => a - b)];
+    document.querySelector(".viewer").innerHTML = reducerArray[0];
   }
 }
 
 function clearAll() {
   document.querySelector(".viewer").innerHTML = 0;
-  arr = [];
+  reducerArray = [];
   sum = 0;
   reference = "";
 }
-
-
-
-
-
