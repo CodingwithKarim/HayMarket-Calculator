6bb9085ac4d4aba0134e88c7ac81ca1fd5e6920f
Karim Hassan <75917863+CodingwithKarim@users.noreply.github.com>
2022-01-29 22:43:09 -0500

    update

---
+++ handleErrors.js
@@ -0,0 +1,4 @@
+function handleErrors(){
+    alert('This is borderline cyberbullying and I think you need a lecture.')
+    location.href = 'https://www.unicef.org/end-violence/how-to-stop-cyberbullying';
+  }
